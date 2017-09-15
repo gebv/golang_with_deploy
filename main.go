575fe123f094acf52ff8bb38340d776e9351aec0
Egor Bachurin <egor.workroom@gmail.com>
2017-09-16 03:54:23 +0600

    fix deploy checker

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 var VERSION = "dev"
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{
 		"verions": VERSION,
 		"path":    r.URL.Path,
